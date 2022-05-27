a92f10c8b0c75292dffbe7232d7a2d6cde32e39f
Luna Rodríguez <RLUNARO@mapfre.net>
2022-05-27 10:14:21 +0200

    Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header } from './header/header.component';
+import { Footer } from './footer/footer.component';
+import { IngredientComponent } from './component/ingredient/ingredient.component';
+import { RecipeListComponent } from './component/recipe-list/recipe-list.component';
+import { ShoppingItemEditComponent } from './component/shopping-item-edit/shopping-item-edit.component';
+import { ShoppingListComponent } from './component/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header, 
+    Footer, 
+    IngredientComponent, 
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingItemEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/ingredient/ingredient.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector : 'app-ingredient', 
+  templateUrl: './ingredient.component.html'
+})
+export class IngredientComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
