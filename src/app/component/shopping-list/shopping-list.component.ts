a6d183b1c48c2d60bcc02f6eb5dcd92cc757445d
Raul Luna <superman_ha_muerto@yahoo.com>
2022-06-07 16:32:02 +0200

    seguimos con el curso

--- src/app/component/shopping-list/shopping-list.component.ts
+++ src/app/component/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  public ingredients = [
+    new Ingredient( 'apples', 5 ), 
+    new Ingredient( 'Tomatoes', 6 )
+  ];
 
   constructor() { }
 
